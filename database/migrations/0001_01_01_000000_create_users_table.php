144e6aea240aad7ba521fe45db1804739f74a52a
AmirhossseinBabaei <ab8622822@gmail.com>
2026-02-27 08:27:10 +0330

    adding users table migration to project

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('phone_number', 20)->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password_hash');
+            $table->enum('status', ['active', 'blocked', 'inactive']);
             $table->rememberToken();
             $table->timestamps();
         });
