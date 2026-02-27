b3d4758a041733172fd8aea362f0f652d89689ba
AmirhossseinBabaei <ab8622822@gmail.com>
2026-02-27 08:56:05 +0330

    cleaning of providers migration

--- database/migrations/2026_02_27_045755_create_providers_table.php
+++ database/migrations/2026_02_27_045755_create_providers_table.php
@@ -13,12 +13,17 @@ return new class extends Migration
     {
         Schema::create('providers', function (Blueprint $table) {
             $table->id();
+
             $table->string('name', 50);
             $table->string('website', 500);
             $table->string('api_base_url', 500);
+
             $table->text('description')->nullable();
+
             $table->enum('status', ['active', 'inactive'])->default('active');
-            $table->integer('priority')->default('0');
+
+            $table->integer('priority')->default(0);
+
             $table->timestamps();
             $table->softDeletes();
         });
